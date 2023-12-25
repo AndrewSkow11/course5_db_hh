7f5881e663c770a7f1e1962eec6e12ac5064752e
Andrew Skow <skovorodnikov11@gmail.com>
2023-12-25 18:28:28 +0300

    небрежное оформление, но норм функциональность

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
+from tabulate import tabulate
 from src.db_manager import DBManager
 from src.hh_api_manager import HhAPIManager
 
+
 # подготовка к работе (создание базы данных и таблиц)
-# db_manager = DBManager()
+db_manager = DBManager()
 # db_manager.create_database()
 # db_manager.create_tables()
 
 # Создание списка работодателей
-list_of_employers = HhAPIManager.get_list_employers()
+# list_of_employers = HhAPIManager.get_list_employers()
 # print(list_of_employers)
 
-hh = HhAPIManager.add_to_table()
-#
-# input("Для получения список всех компаний и количество вакансий у каждой компании нажмите ENTER\n")
-# db_manager.get_companies_and_vacancies_count()
+# hh = HhAPIManager.add_to_table()
+
+task = input(
+            "Введите 1, чтобы получить список всех компаний и количество вакансий у каждой компании\n"
+            "Введите 2, чтобы получить список всех вакансий с указанием названия компании, "
+            "названия вакансии и зарплаты и ссылки на вакансию\n"
+            "Введите 3, чтобы получить среднюю зарплату по вакансиям\n"
+            "Введите 4, чтобы получить список всех вакансий, у которых зарплата выше средней по всем вакансиям\n"
+            "Введите 5, чтобы получить список всех вакансий, в названии которых содержатся переданные в метод слова\n"
+            "Введите Стоп, чтобы завершить работу\n"
+            ">> "
+        )
+
+if task == "Стоп":
+    exit()
+
+elif task == '1':
+    print(tabulate(db_manager.get_companies_and_vacancies_count()))
+
+elif task == '2':
+    print(tabulate(db_manager.get_all_vacancies()))
+
+elif task == '3':
+    print( tabulate( db_manager.get_avg_salary()))
+
+elif task == '4':
+    print(tabulate(db_manager.get_vacancies_with_higher_salary()))
+
+elif task == '5':
+    keyword = input('Введите ключевое слово: ')
+    print(tabulate(db_manager.get_vacancies_with_keyword(keyword)))
+
+else:
+    print('Неправильный запрос')
+
+
+
+
+
+
+
+
 
 
 
--- src/db_manager.py
+++ src/db_manager.py
@@ -92,55 +92,55 @@ class DBManager:
             conn.commit()
         return result
 
-    # def get_all_vacancies(self):
-    #     '''Метод получает список всех вакансий с указанием названия компании,
-    #     названия вакансии и зарплаты и ссылки на вакансию'''
-    #     with psycopg2.connect(host="localhost", database="course_work_5",
-    #                           user="postgres", password="12345") as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute(f"SELECT employers.company_name, vacancies.vacancies_name, "
-    #                         f"vacancies.payment, vacancies_url "
-    #                         f"FROM employers "
-    #                         f"JOIN vacancies USING (employer_id)")
-    #             result = cur.fetchall()
-    #         conn.commit()
-    #     return result
-    #
-    #
-    # def get_avg_salary(self):
-    #     '''Метод получает среднюю зарплату по вакансиям'''
-    #     with psycopg2.connect(host="localhost", database="course_work_5",
-    #                           user="postgres", password="12345") as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute(f"SELECT AVG(payment) as avg_payment FROM vacancies ")
-    #             result = cur.fetchall()
-    #         conn.commit()
-    #     return result
-    #
-    #
-    # def get_vacancies_with_higher_salary(self):
-    #     '''Метод получает список всех вакансий,
-    #     у которых зарплата выше средней по всем вакансиям'''
-    #     with psycopg2.connect(host="localhost", database="course_work_5",
-    #                           user="postgres", password="12345") as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute(f"SELECT * FROM vacancies "
-    #                         f"WHERE payment > (SELECT AVG(payment) FROM vacancies) ")
-    #             result = cur.fetchall()
-    #         conn.commit()
-    #     return result
-    #
-    #
-    # def get_vacancies_with_keyword(self, keyword):
-    #     '''Метод получает список всех вакансий,
-    #     в названии которых содержатся переданные в метод слова'''
-    #     with psycopg2.connect(host="localhost", database="course_work_5",
-    #                           user="postgres", password="12345") as conn:
-    #         with conn.cursor() as cur:
-    #             cur.execute(f"SELECT * FROM vacancies "
-    #                         f"WHERE lower(vacancies_name) LIKE '%{keyword}%' "
-    #                         f"OR lower(vacancies_name) LIKE '%{keyword}'"
-    #                         f"OR lower(vacancies_name) LIKE '{keyword}%';")
-    #             result = cur.fetchall()
-    #         conn.commit()
-    #     return result
+    def get_all_vacancies(self):
+        '''Метод получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию'''
+        with psycopg2.connect(host="localhost", database="hh_vacancies",
+                              user="postgres", password="99-100") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT employers.company_name, vacancies.vacancies_name, "
+                            f"vacancies.payment, vacancies_url "
+                            f"FROM employers "
+                            f"JOIN vacancies USING (employer_id)")
+                result = cur.fetchall()
+            conn.commit()
+        return result
+
+
+    def get_avg_salary(self):
+        '''Метод получает среднюю зарплату по вакансиям'''
+        with psycopg2.connect(host="localhost", database="hh_vacancies",
+                              user="postgres", password="99-100") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT AVG(payment) as avg_payment FROM vacancies ")
+                result = cur.fetchall()
+            conn.commit()
+        return result
+
+
+    def get_vacancies_with_higher_salary(self):
+        '''Метод получает список всех вакансий,
+        у которых зарплата выше средней по всем вакансиям'''
+        with psycopg2.connect(host="localhost", database="hh_vacancies",
+                              user="postgres", password="99-100") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM vacancies "
+                            f"WHERE payment > (SELECT AVG(payment) FROM vacancies) ")
+                result = cur.fetchall()
+            conn.commit()
+        return result
+
+
+    def get_vacancies_with_keyword(self, keyword):
+        '''Метод получает список всех вакансий,
+        в названии которых содержатся переданные в метод слова'''
+        with psycopg2.connect(host="localhost", database="hh_vacancies",
+                              user="postgres", password="99-100") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM vacancies "
+                            f"WHERE lower(vacancies_name) LIKE '%{keyword}%' "
+                            f"OR lower(vacancies_name) LIKE '%{keyword}'"
+                            f"OR lower(vacancies_name) LIKE '{keyword}%';")
+                result = cur.fetchall()
+            conn.commit()
+        return result
